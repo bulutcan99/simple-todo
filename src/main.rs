ea1823baa83f461f60c489f5790503d5d2d37628
bulutcan99 <gocerbulutcan@gmail.com>
2024-05-29 19:15:33 +0300

    feat: added impl

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+pub struct Todo {
+	pub description: String,
+	pub done: bool,
 }
+
+pub struct File {
+	pub username: String,
+	pub todo: Vec<Todo>,
+}
+
+impl File {
+	pub fn new(username: &str) -> Result<Self, String> {
+		match username {
+			"" => Err("Username cannot be empty".to_string()),
+			_ => Ok(Self {
+				username: username.to_string(),
+				todo: Vec::new(),
+			}),
+		}
+	}
+}
+
+fn main() {
+	let username: &str = "Bulut";
+	let file = File::new(username).unwrap();
+}
+
+
 
